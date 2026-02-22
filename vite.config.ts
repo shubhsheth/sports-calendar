bbc7e4dff680a8d03588a161bb3e1fae20b77b4a
Shubh Sheth <shubhsheth8@gmail.com>
2026-02-21 18:42:34 -0600

    Github pages config

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "sports-calendar",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
